0dfc9a6bd62cd6748d5107aecf5972da0a31036b
Affan-mulla <affanmulla077@gmail.com>
2024-12-22 17:35:09 +0530

    Question 4 Completed

--- Assignment-1/src/answers/Q4.java
+++ Assignment-1/src/answers/Q4.java
@@ -1,10 +1,26 @@
 package answers;
+
 import java.util.*;
+
 public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String b = sc.next();
 
-        
+        System.out.println("Enter No :");
+        int no = sc.nextInt();
+        int ch = 0;
+        String[] split = String.valueOf(no).split("");
+
+        for (int i = 0; i < split.length; i++) {
+            ch += Math.pow(Integer.valueOf(split[i]), split.length);
+            System.out.println(ch);
+        }
+
+        if (ch == no) {
+            System.out.println(ch + " is a Armstrong Number");
+        } else {
+            System.out.println(ch + " is not a Armstrong Number");
+        }
+
     }
 }
