fecd30f0de8df9740493247ef1cf6889328018e2
Affan-mulla <affanmulla077@gmail.com>
2025-01-13 18:09:21 +0530

    Q3

--- Assignment_2/src/A1/Employee.java
+++ Assignment_2/src/A1/Employee.java
@@ -1,9 +1,12 @@
 package A1;
 
+import java.util.Scanner;
+
 public class Employee {
     int YOJ;
     String name;
     String Address;
+
     Employee(String name, int YOJ, String Address) {
         this.name = name;
         this.YOJ = YOJ;
@@ -11,15 +14,41 @@ public class Employee {
     }
 
     void printEmpDetails() {
-        System.out.println(name+"\t"+YOJ+"\t"+Address);
+        System.out.println(name + "\t" + YOJ + "\t" + Address);
+    }
+
+    static String takeName() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name:");
+        String name = sc.next();
+
+        return name;
+    }
+
+    static int takeYear() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Age :");
+        int year = sc.nextInt();
+
+        return year;
+    }
+
+    static String takeAddress() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Address :");
+        String Address = sc.next();
+
+        return Address;
     }
 
     public static void main(String[] args) {
-        System.out.println("Name\tYear of Joining\tAddress"); 
-        Employee E1 = new Employee("Robert", 1994, "64C WallsStreat");
-        Employee E2 = new Employee("Sam", 2000, "68D WallsStreat");
-        Employee E3 = new Employee("Robert", 1999, "26B WallsStreat");
 
+        Employee E1 = new Employee(takeName(), takeYear(), takeAddress());
+        Employee E2 = new Employee(takeName(), takeYear(), takeAddress());
+        Employee E3 = new Employee(takeName(), takeYear(), takeAddress());
+
+        System.out.println("Name\tYear of Joining\tAddress");
         E1.printEmpDetails();
         E2.printEmpDetails();
         E3.printEmpDetails();
